4f50d52dab20bc7216e28a08bed6429f50836f2f
shirsho-home <bsse1106@iit.du.ac.bd>
2024-03-23 14:02:31 +0600

    auth done

--- src/components/forms/signinForm/SignInForm.tsx
+++ src/components/forms/signinForm/SignInForm.tsx
@@ -35,103 +35,112 @@ export const SignInForm: React.FC = () => {
 
   return (
     <div className='flex min-h-screen items-center justify-center bg-gray-100'>
-      <div className='w-full max-w-md rounded-lg bg-white p-8 shadow-md'>
-        <div className='mb-8 flex items-center justify-center'>
-          <div className='mb-4'>
-            <Image
-              src='/amarneer.png'
-              alt='logo image'
-              height={35}
-              width={130}
-            />
-          </div>
-        </div>
-        <h2 className='mb-4 text-lg font-bold text-purple-900'>
-          Log in to your Account
-        </h2>
-        <p className='mb-6 text-sm text-gray-600'>
-          Use your work email or phone number to log in to your account
-        </p>
-        <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
-          <div className='flex flex-col'>
-            <label
-              htmlFor='email'
-              className='text-sm font-semibold text-gray-900'
-            >
-              Email or Phone Number
-            </label>
-            <input
-              id='email'
-              type='text'
-              className={`border ${errors.email ? 'border-red-500' : 'border-gray-300'} rounded-lg px-4 py-2 focus:border-blue-500 focus:outline-none`}
-              {...register('email', { required: true })}
-            />
-            {errors.email && (
-              <p className='text-xs text-red-500'>Email is required</p>
-            )}
-          </div>
-          <div className='relative flex flex-col'>
-            <label
-              htmlFor='password'
-              className='text-sm font-semibold text-gray-900'
-            >
-              Password
-            </label>
-            <div className='flex items-center'>
-              <input
-                id='password'
-                type={showPassword ? 'text' : 'password'}
-                className={`border ${errors.password ? 'border-red-500' : 'border-gray-300'} w-full rounded-lg px-4 py-2 focus:border-blue-500 focus:outline-none`}
-                {...register('password', { required: true })}
+      <div className='z-10 flex'>
+        <div className='w-full max-w-md rounded-l-lg bg-white p-8 shadow-md'>
+          <div className='justify-left mb-8 flex items-center'>
+            <div className='mb-4'>
+              <Image
+                src='/amarneer.png'
+                alt='logo image'
+                height={35}
+                width={130}
               />
-
-              <button
-                type='button'
-                onClick={() => setShowPassword(!showPassword)}
-                className='absolute inset-y-0 right-0 flex items-center px-4'
-              >
-                {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
-              </button>
             </div>
-            {errors.password && (
-              <p className='text-xs text-red-500'>Password is required</p>
-            )}
           </div>
-          <div className='flex items-center justify-between'>
-            <label
-              htmlFor='remember'
-              className='text-sm font-semibold text-gray-900'
-            >
+          <h2 className='mb-4 text-lg font-bold text-purple-900'>
+            Log in to your Account
+          </h2>
+          <p className='mb-6 text-sm text-gray-600'>
+            Use your work email or phone number to log in to your account
+          </p>
+          <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
+            <div className='flex flex-col'>
+              <label
+                htmlFor='email'
+                className='text-sm font-semibold text-gray-900'
+              >
+                Email or Phone Number
+              </label>
               <input
-                id='remember'
-                type='checkbox'
-                className='mr-2'
-                {...register('rememberMe')}
+                id='email'
+                type='text'
+                className={`border ${errors.email ? 'border-red-500' : 'border-gray-300'} rounded-lg px-4 py-2 focus:border-blue-500 focus:outline-none`}
+                {...register('email', { required: true })}
               />
-              Remember me
-            </label>
-            <a
-              href='./forgotpassword'
-              className='text-sm font-semibold text-blue-600'
+              {errors.email && (
+                <p className='text-xs text-red-500'>Email is required</p>
+              )}
+            </div>
+            <div className='relative flex flex-col'>
+              <label
+                htmlFor='password'
+                className='text-sm font-semibold text-gray-900'
+              >
+                Password
+              </label>
+              <div className='flex items-center'>
+                <input
+                  id='password'
+                  type={showPassword ? 'text' : 'password'}
+                  className={`border ${errors.password ? 'border-red-500' : 'border-gray-300'} w-full rounded-lg px-4 py-2 focus:border-blue-500 focus:outline-none`}
+                  {...register('password', { required: true })}
+                />
+
+                <button
+                  type='button'
+                  onClick={() => setShowPassword(!showPassword)}
+                  className='absolute inset-y-0 right-0 flex items-center px-4'
+                >
+                  {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
+                </button>
+              </div>
+              {errors.password && (
+                <p className='text-xs text-red-500'>Password is required</p>
+              )}
+            </div>
+            <div className='flex items-center justify-between'>
+              <label
+                htmlFor='remember'
+                className='text-sm font-semibold text-gray-900'
+              >
+                <input
+                  id='remember'
+                  type='checkbox'
+                  className='mr-2'
+                  {...register('rememberMe')}
+                />
+                Remember me
+              </label>
+              <a
+                href='./forgotpassword'
+                className='text-sm font-semibold text-blue-600'
+              >
+                Forget Password?
+              </a>
+            </div>
+            <button
+              type='submit'
+              className='mt-4 w-full rounded-lg bg-orange-500 py-2 font-semibold text-white'
             >
-              Forget Password?
+              Log In
+            </button>
+          </form>
+          <div className='mt-4 flex items-center'>
+            <p className='text-sm text-gray-600'>Do not have an account? </p>
+            <a
+              href='./signup'
+              className='ml-1 text-sm font-semibold text-blue-600'
+            >
+              Sign Up
             </a>
           </div>
-          <button
-            type='submit'
-            className='mt-4 w-full rounded-lg bg-orange-500 py-2 font-semibold text-white'
-          >
-            Log In
-          </button>
-        </form>
-        <div className='mt-4 flex items-center'>
-          <p className='text-sm text-gray-600'>Do not have an account? </p>
-          <a
-            href='./signup'
-            className='ml-1 text-sm font-semibold text-blue-600'
-          >
-            Sign Up
-          </a>
+        </div>
+        <div className='w-full max-w-md overflow-hidden rounded-r-lg bg-black shadow-md'>
+          <img
+            src={'/towers.png'}
+            alt={''}
+            className='h-full w-full opacity-90'
+          />
         </div>
       </div>
     </div>
--- src/components/forms/signupForm/SignupForm.tsx
+++ src/components/forms/signupForm/SignupForm.tsx
@@ -29,7 +29,7 @@ const SignUpForm: React.FC = () => {
         toast.success(`An email sent ${data.email}`);
         router.push(`/otp?email=${data.email}&t=new`);
       } else {
-        toast.error('Sign up failed1. Please try again.');
+        toast.error('Sign up failed. Please try again.');
       }
     } catch (error: any) {
       console.log(error);
@@ -43,115 +43,143 @@ const SignUpForm: React.FC = () => {
   return (
     <div className='relative flex min-h-screen items-center justify-center bg-gray-100'>
       {/* Background Image */}
-      <div className='absolute inset-y-0 right-0 hidden w-1/2 bg-amraneer bg-cover bg-center md:block'></div>
+      {/* <div className='absolute inset-y-0 right-0 hidden w-1/2 bg-amraneer bg-cover bg-center md:block'></div> */}
 
       <div className='z-10 flex min-h-screen items-center justify-center bg-gray-100'>
-        <div className='w-full max-w-md rounded-lg bg-white p-8 shadow-md'>
-          <div className='mb-8 flex items-center justify-center'>
-            <div className='mb-4'>
-              <Image
-                src='/amarneer.png'
-                alt='logo image'
-                width={163}
-                height={43.81}
-              />
-            </div>
-          </div>
-          <h2 className='mb-4 text-lg font-bold text-purple-900'>
-            Sign up to your Account
-          </h2>
-          <p className='mb-6 text-sm text-gray-600'>
-            Create your account with specific email/mobile number; we can easily
-            contact with you
-          </p>
-          <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
-            <div className='flex flex-col'>
-              <label
-                htmlFor='email'
-                className='text-sm font-semibold text-gray-900'
-              >
-                Email or Phone Number
-              </label>
-              <div className='flex items-center rounded-lg border border-gray-300'>
-                <input
-                  id='email'
-                  type='text'
-                  className={`px-4 py-2 focus:outline-none ${errors.email ? 'border-red-500' : 'border-blue-500'}`}
-                  {...register('email', { required: true })}
+        <div className='z-10 flex'>
+          <div className='w-full max-w-md rounded-l-lg bg-white p-8 shadow-md'>
+            <div className='justify-left mb-8 flex items-center'>
+              <div className='mb-4'>
+                <Image
+                  src='/amarneer.png'
+                  alt='logo image'
+                  height={35}
+                  width={130}
                 />
               </div>
-              {errors.email && (
-                <p className='text-xs text-red-500'>Email is required</p>
-              )}
             </div>
-            <div className='relative flex flex-col'>
-              <label
-                htmlFor='password'
-                className='text-sm font-semibold text-gray-900'
-              >
-                Password
-              </label>
-              <div className='flex items-center rounded-lg border border-gray-300'>
+            <h2 className='mb-4 text-lg font-bold text-purple-900'>
+              Sign up to your Account
+            </h2>
+            <p className='mb-6 text-sm text-gray-600'>
+              Create your account with specific email/mobile number; we can
+              easily contact with you
+            </p>
+            <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
+              <div className='flex flex-col'>
+                <label
+                  htmlFor='name'
+                  className='text-sm font-semibold text-gray-900'
+                >
+                  Your Name
+                </label>
+                <div className='flex items-center rounded-lg border border-gray-300'>
+                  <input
+                    id='name'
+                    type='text'
+                    className={`px-4 py-2 focus:outline-none ${errors.name ? 'border-red-500' : 'border-blue-500'}`}
+                    {...register('name', { required: true })}
+                  />
+                </div>
+                {errors.name && (
+                  <p className='text-xs text-red-500'>Name is required</p>
+                )}
+              </div>
+              <div className='flex flex-col'>
+                <label
+                  htmlFor='email'
+                  className='text-sm font-semibold text-gray-900'
+                >
+                  Email or Phone Number
+                </label>
+                <div className='flex items-center rounded-lg border border-gray-300'>
+                  <input
+                    id='email'
+                    type='text'
+                    className={`px-4 py-2 focus:outline-none ${errors.email ? 'border-red-500' : 'border-blue-500'}`}
+                    {...register('email', { required: true })}
+                  />
+                </div>
+                {errors.email && (
+                  <p className='text-xs text-red-500'>Email is required</p>
+                )}
+              </div>
+              <div className='relative flex flex-col'>
+                <label
+                  htmlFor='password'
+                  className='text-sm font-semibold text-gray-900'
+                >
+                  Password
+                </label>
+                <div className='flex items-center rounded-lg border border-gray-300'>
+                  <input
+                    id='password'
+                    type={showPassword ? 'text' : 'password'}
+                    className={`px-4 py-2 focus:outline-none ${errors.password ? 'border-red-500' : 'border-blue-500'}`}
+                    {...register('password', { required: true })}
+                  />
+                  <button
+                    type='button'
+                    onClick={() => setShowPassword(!showPassword)}
+                    className='absolute inset-y-0 right-0 flex items-center px-4'
+                  >
+                    {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
+                  </button>
+                </div>
+                {errors.password && (
+                  <p className='text-xs text-red-500'>Password is required</p>
+                )}
+              </div>
+              <div className='relative'>
+                <label htmlFor='confirmPassword' className='mt-4 text-gray-800'>
+                  Confirm Password
+                </label>
                 <input
-                  id='password'
+                  id='confirmPassword'
                   type={showPassword ? 'text' : 'password'}
-                  className={`px-4 py-2 focus:outline-none ${errors.password ? 'border-red-500' : 'border-blue-500'}`}
-                  {...register('password', { required: true })}
+                  autoComplete='new-password'
+                  {...register('confirmPassword', {
+                    required: true,
+                    validate: (value) =>
+                      value === confirmPassword || 'The passwords do not match',
+                  })}
+                  className='mt-2 block w-full rounded-md border border-blue-500 bg-white px-4 py-2 text-gray-700 focus:border-2 focus:outline-none focus:ring-0'
                 />
                 <button
                   type='button'
                   onClick={() => setShowPassword(!showPassword)}
-                  className='absolute inset-y-0 right-0 flex items-center px-4'
+                  className='absolute inset-y-0 right-0 top-8 flex items-center px-2 focus:outline-none'
                 >
                   {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
                 </button>
+                {errors.confirmPassword && (
+                  <p className='text-red-500'>Passwords do not match</p>
+                )}
               </div>
-              {errors.password && (
-                <p className='text-xs text-red-500'>Password is required</p>
-              )}
-            </div>
-            <div className='relative'>
-              <label htmlFor='confirmPassword' className='mt-4 text-gray-800'>
-                Confirm Password
-              </label>
-              <input
-                id='confirmPassword'
-                type={showPassword ? 'text' : 'password'}
-                autoComplete='new-password'
-                {...register('confirmPassword', {
-                  required: true,
-                  validate: (value) =>
-                    value === confirmPassword || 'The passwords do not match',
-                })}
-                className='mt-2 block w-full rounded-md border border-red-500 bg-white px-4 py-2 text-gray-700 focus:border-2 focus:outline-none focus:ring-0'
-              />
-              <button
-                type='button'
-                onClick={() => setShowPassword(!showPassword)}
-                className='absolute inset-y-0 right-0 top-8 flex items-center px-2 focus:outline-none'
-              >
-                {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
-              </button>
-              {errors.confirmPassword && (
-                <p className='text-red-500'>Passwords do not match</p>
-              )}
-            </div>
 
-            <button
-              type='submit'
-              className='mt-4 w-full rounded-lg bg-orange-500 py-2 font-semibold text-white'
-            >
-              Sign Up
-            </button>
-          </form>
-          <div className='mt-4 flex items-center'>
-            <p className='text-sm text-gray-600'>Already have an account?</p>
-            <a
-              href='./signin'
-              className='ml-1 text-sm font-semibold text-blue-600'
-            >
-              Sign in
-            </a>
+              <button
+                type='submit'
+                className='mt-4 w-full rounded-lg bg-orange-500 py-2 font-semibold text-white'
+              >
+                Sign Up
+              </button>
+            </form>
+            <div className='mt-4 flex items-center'>
+              <p className='text-sm text-gray-600'>Already have an account?</p>
+              <a
+                href='./signin'
+                className='ml-1 text-sm font-semibold text-blue-600'
+              >
+                Sign in
+              </a>
+            </div>
+          </div>
+          <div className='w-full max-w-md overflow-hidden rounded-r-lg bg-black shadow-md'>
+            <img
+              src={'/towers.png'}
+              alt={''}
+              className='h-full w-full opacity-90'
+            />
           </div>
         </div>
       </div>
